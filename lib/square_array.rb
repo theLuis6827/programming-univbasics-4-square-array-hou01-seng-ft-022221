eef071d103d205a7def45d48ff245048860f36c6
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-10 04:10:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def square_array(numbers)
   count = 0
   new_numbers = []
@@ -7,3 +8,14 @@ def square_array(numbers)
   end
   return new_numbers
 end 
+=======
+def square_array(array)
+  numbers = [1, 2, 3]
+  new_numbers = []
+  while numbers < array.length do
+    new_numbers.push(array[numbers].to_s)
+    numbers = Math.sqrt(numbers)
+  end
+  new_numbers
+end
+>>>>>>> 79c760947a8670fcb3c1930a5539b2ef644a2365
